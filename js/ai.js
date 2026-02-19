4ff668811b5279d79bdbfa60cb833a639baf486c
hexzagon <kendrew_adriano@pluit.ipeka.sch.id>
2026-02-19 07:42:57 +0700

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,11 +1,1 @@
-function updateEnemies() {
-  enemies.forEach(enemy => {
-    if (enemy.userData.state === "alert") {
-      const dx = camera.position.x - enemy.position.x;
-      const dz = camera.position.z - enemy.position.z;
 
-      enemy.position.x += dx * 0.01;
-      enemy.position.z += dz * 0.01;
-    }
-  });
-}
