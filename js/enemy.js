85e72bcc772c4acd9abc346b9acc5d912ed2e047
hexzagon <kendrew_adriano@pluit.ipeka.sch.id>
2026-02-19 07:43:14 +0700

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,16 +1,1 @@
-function spawnEnemy(x, y, z) {
-  const enemy = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 2, 1),
-    new THREE.MeshStandardMaterial({ color: 0xff0000 })
-  );
 
-  enemy.position.set(x, y, z);
-  enemy.userData = {
-    health: 100,
-    state: "idle",
-    awareTimer: 0
-  };
-
-  scene.add(enemy);
-  enemies.push(enemy);
-}
